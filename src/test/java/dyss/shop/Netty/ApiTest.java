edc188a9f52c3b323cfe91a8f465c74344dde438
root <260874141@qq.com>
2024-07-15 22:43:59 +0800

    feat：初始化仓库

---
+++ src/test/java/dyss/shop/Netty/ApiTest.java
@@ -0,0 +1,16 @@
+package dyss.shop.Netty;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author dyss
+ * @date 2024/7/15 22:38
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@Slf4j
+public class ApiTest {
+}
